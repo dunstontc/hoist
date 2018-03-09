fe07fbc81e482282e21790df94f9c4eeb29b7c5d
Clay Dunston <dunstontc@gmail.com>
2018-03-09 14:35:20 -0600

    Added FiraCode

--- readme.md
+++ readme.md
@@ -1,23 +1,5 @@
-# Make
+# Hoist
 
 ## Resources
-- [Makeheads101: Makefiles for Dummies](Makeheads101: Makefiles for Dummies)
-
-## RHEL Setup
-```sh
-# First clean up everything - open a terminal and execute the following commands :
-sudo subscription-manager remove --all
-sudo subscription-manager unregister
-sudo subscription-manager clean
-
-# Now re-register the system, attach the subscriptions - execute these commands :
-sudo subscription-manager register
-sudo subscription-manager refresh
-sudo subscription-manager attach --auto
-
-# Here are the commands to see which repos are enabled and what can be added :
-# sudo subscription-manager repos --list-enabled to see all actually enabled subscriptions.
-# sudo subscription-manager repos --list to see all subscriptions that are available for you.
-# sudo subscription-manager repos --enable <repo> if you want to add additional repos.
-```
+- Makeheads101: Makefiles for Dummies
 
--- unix/scripps/install.sh
+++ unix/scripps/install.sh
@@ -17,10 +17,30 @@ hoist_debuntu() {
   sudo apt upgrade;
 }
 
+hoist_redhat() {
+  # First clean up everything - open a terminal and execute the following commands :
+  sudo subscription-manager remove --all
+  sudo subscription-manager unregister
+  sudo subscription-manager clean
+
+  # Now re-register the system, attach the subscriptions - execute these commands :
+  sudo subscription-manager register
+  sudo subscription-manager refresh
+  sudo subscription-manager attach --auto
+
+  # Here are the commands to see which repos are enabled and what can be added :
+  # sudo subscription-manager repos --list-enabled to see all actually enabled subscriptions.
+  # sudo subscription-manager repos --list to see all subscriptions that are available for you.
+  # sudo subscription-manager repos --enable <repo> if you want to add additional repos.
+}
+
+
 hoist_arch() {
   # General update
   sudo pacman -Syu
 }
+
+
 
 
 # Teach terminals how to use true color & escape italics
