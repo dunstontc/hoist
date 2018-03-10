324c20d54b1b0e07998ce9d5a11cc5107a7313b9
Clay Dunston <dunstontc@gmail.com>
2018-03-09 23:16:05 -0600

    changed install-screenfetch to install-neofetch

--- unix/scripps/install-screenfetch.sh
+++ unix/scripps/install-screenfetch.sh
@@ -1,2 +1,9 @@
-wget -O ~/bin/screenfetch-dev https://git.io/vaHfR
-chmod +x ~/bin/screenfetch-dev
+# https://github.com/dylanaraps/neofetch/wiki/Installation#latest-git-master-bleeding-edge
+
+mkdir -p ~/tmp;
+cd ~/tmp;
+# wget -O ~/bin/screenfetch-dev https://git.io/vaHfR
+# chmod +x ~/bin/screenfetch-dev
+git clone https://github.com/dylanaraps/neofetch;
+cd neofetch;
+make install;
