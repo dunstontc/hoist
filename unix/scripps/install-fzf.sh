1075b7390b993459110a9746fa1f2779b7e58491
Clay Dunston <dunstontc@gmail.com>
2018-03-10 19:05:16 -0600

    same

--- unix/scripps/install-fzf.sh
+++ unix/scripps/install-fzf.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 # https://github.com/junegunn/fzf#using-git
+
+reset="\e[0m";
+blue="\e[0;34m";
+
+echo "\[${blue}\] Installing Fzf \[${reset}\]"
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
