fff3d826d9c9df5d0b7e5c4a8411ef89b631df82
Clay Dunston <dunstontc@gmail.com>
2018-03-11 17:54:37 -0500

    Go install scripp

--- unix/scripps/install-go.sh
+++ unix/scripps/install-go.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir -p ~/tmp;
 cd ~/tmp;
-curl -LO https://dl.google.com/go/go1.10.linux-amd64.tar.gz
-tar -C /usr/local -xzf go1.10.linux-amd64.tar.gz
+[ -f ~/tmp/go1.10.linux-amd64.tar.gz ] || curl -LO https://dl.google.com/go/go1.10.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.10.linux-amd64.tar.gz
 
