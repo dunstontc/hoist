7295ce45b938e28402c46182a69988a3c40ba11d
Clay Dunston <dunstontc@gmail.com>
2018-03-13 00:09:50 -0500

    Testing some more formatting

--- unix/scripps/install-screenfetch.sh
+++ unix/scripps/install-screenfetch.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
 Reset='\033[0m'
-Blue='\033[0;34m'
+Blue='\033[38;2;78;201;176m'
 
-echo -e "${Blue}=== Installing Screenfetch ===${Reset}"
+inform() {
+  printf "${Blue}=== $1 ===${Reset}\n";
+}
+
+inform "Installing Screenfetch";
 
 mkdir -p ~/bin;
 wget -O ~/bin/screenfetch-dev https://git.io/vaHfR;
 chmod +x ~/bin/screenfetch-dev;
+
+clear;
+screenfetch-dev;
--- unix/scripps/zsh-plugins.sh
+++ unix/scripps/zsh-plugins.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 Reset='\033[0m'
-Blue='\033[0;34m'
+Blue='\033[38;2;78;201;176m'
+
+inform() {
+  printf "${Blue}=== $1 ===${Reset}\n";
+}
 
 mkdir -p "$HOME/.zsh/plugins";
 
-inform() {
-  printf "${Blue}=== Installing $1 ===${Reset}\n";
-}
-
-inform "Autosuggestions"
+# clear;
+inform "Installing Autosuggestions"
 git clone https://github.com/zsh-users/zsh-autosuggestions.git          ~/.zsh/plugins/zsh-autosuggestions
-inform "Syntax Highlighting"
+inform "Installing Syntax Highlighting"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git      ~/.zsh/plugins/zsh-syntax-highlighting
-inform "History Substring Search"
+inform "Installing History Substring Search"
 git clone https://github.com/zsh-users/zsh-history-substring-search.git ~/.zsh/plugins/zsh-history-substring-search
-inform "fzf-marks"
+inform "Installing fzf-marks"
 git clone https://github.com/urbainvaes/fzf-marks.git                   ~/.zsh/plugins/fzf-marks
-inform "Completions"
+inform "Installing Completions"
 git clone https://github.com/zsh-users/zsh-completions.git              ~/.zsh/plugins/zsh-completions
-clear;
-printf "${Blue}=== Done! ===${Reset}\n";
+inform "Done!"
