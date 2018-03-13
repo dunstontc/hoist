fd0aa40a83f23689580321623db0bbcb96293130
Clay Dunston <dunstontc@gmail.com>
2018-03-13 00:38:59 -0500

    more of the same

--- unix/scripps/fonts.sh
+++ unix/scripps/fonts.sh
@@ -1,7 +1,16 @@
 # https://wiki.debian.org/Fonts
 
+Reset='\033[0m'
+Blue='\033[38;2;78;201;176m'
+
+inform() {
+  printf "${Blue}=== $1 ===${Reset}\n";
+}
+
 mkdir -p ~/.fonts;
 
-cp -r ~/hoist/fonts/ubuntu-mono-nerdfont ~/.fonts/ubuntu-mono-nerdfont
+cp -r ~/hoist/settings/fonts/ubuntu-mono-nerdfont ~/.fonts/ubuntu-mono-nerdfont
 
 fc-cache -v
+
+inform "Fonts Updated";
